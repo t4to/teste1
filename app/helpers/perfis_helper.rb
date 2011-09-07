0bc4c140680bf2c6d26a9e60b8534bdbb73b6ad0
Tato <tato.rodrigues@gmail.com>
2011-09-07 19:14:50 -0300

    Perfil

--- app/helpers/perfis_helper.rb
+++ app/helpers/perfis_helper.rb
@@ -3,5 +3,14 @@ module PerfisHelper
 	def get_perfis_from_current_admin(current_admin)
 		categs = get_categorias_from_current_admin(current_admin)
 		#...
+		perf = Perfil.new
+		return perf if current_admin == nil
+
+		categs.each do |ct|
+			perf << Perfil.find_by_categoria_id(ct.id)
+		end
+		
+		#Esta retornando array?
+		return perf
 	end
 end
